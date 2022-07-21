2c26ad5dab5be6312bd73bdd9d02e59e4ab3cce7
Denis Blinov <blinovda1@mail.ru>
2022-07-21 21:18:21 +0300

    add task 4

---
+++ Seminar_4/task.py
@@ -0,0 +1,51 @@
+# Задача 1
+# Задайте строку из набора чисел. Напишите программу,
+# которая покажет большее и меньшее число.
+# В качестве символа-разделителя используйте пробел.
+
+# file_path = r'Seminar_4\file.txt'
+# with open(file_path, 'r') as f:
+#     my_str = f.read()
+# print(my_str)
+# my_str = my_str.split()
+# print(my_str)
+# for i in range(len(my_str)):
+#     my_str[i] = int(my_str[i])
+# print(my_str)
+# print(f'max -> {max(my_str)}')
+# print(f'min -> {min(my_str)}')
+
+
+# Задача 2
+# Найдите корни квадратного уравнения:
+# ax^2 + bx + c = 0 двумя способами
+# 1) с помощью математических формул нахождения корней кв ур
+# 2)
+
+# path = r'Seminar_4\file2.txt'
+# with open(path, 'r') as my_file:
+#     data = my_file.read()
+
+# print(data)
+# data = data.split()
+# print(data)
+
+# data = data[:-2]
+# print(data)
+
+# new_data = [data[0], data[1] + data[2], data[3] + data[4]]
+# print(new_data)
+
+# new_data = [int(data[0][:-3]), int((data[1] + data[2])[:-1]), int(data[3] + data[4])]
+# print(new_data)
+
+# # D = b^2 - 4ac
+
+# d = new_data[1]**2 - 4 * new_data[0] * new_data[2]
+# print(d)
+
+# # x = ((-b +- (d^(1/2))) / (2*a))
+# # x_1 = (-new_data[1] + d**0,5) / (2 * new_data[0])
+# # x_2 = (-new_data[1] - d**0,5) / (2 * new_data[0])
+# # print(x_1, x_2)
+
