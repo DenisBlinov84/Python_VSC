fd3e2adfaefe0dce06a4b6e43b78055700822b25
Denis Blinov <blinovda1@mail.ru>
2022-07-27 16:52:43 +0300

    add info

--- Seminar_5/info.py
+++ Seminar_5/info.py
@@ -15,13 +15,13 @@
 
 
 
-# lambda:
+# lambda: простое дйствие
 # f = lambda x: x**2 if x>10 else x**3
 # print(f(12))
 
 
 
-# map:
+# map: если есть набор элементов и их нужно прогнать через функцию(сокращаем for и вызов функции)
 # f = lambda x: x**2 if x>10 else x**3
 # my_list = [12, 54, 3, 65]
 
@@ -30,7 +30,7 @@
 
 
 
-# filter:
+# filter: (отфильтровать условие, сокращаем for, if и вызов функции)
 # f = lambda x: True if x>10 else False
 # my_list = [12, 54, 3, 65]
 # res = list(filter(f, my_list)) 
@@ -38,17 +38,28 @@
 
 
 
-# map:
+# map: (компанует новые колекции)
 # my_list_1 = [12, 54, 3, 65]
 # my_list_2 = [15, 76, 1, 98]
 # my_list_3 = [65, 68, 8, 23]
 # res = list(zip(my_list_1, my_list_2, my_list_3))
 # print(res)
 
-my_list_1 = ['A', 'B', 'C', 'D']
-my_list_2 = [15, 76, 1, 98]
-res = dict(zip(my_list_1, my_list_2))
-print(res)
+# my_list_1 = ['A', 'B', 'C', 'D']
+# my_list_2 = [15, 76, 1, 98]
+# res = dict(zip(my_list_1, my_list_2)) # в словарях можно компановать только 2 списка
+# print(res)
+
+
+
+# enumerate (нумерует новые колекции)
+# my_list_1 = ['A', 'B', 'C', 'D']
+# res = list(enumerate(my_list_1))
+# print(res)
+
+# data = list(map(int, input().split()))
+
+
 
 
 
